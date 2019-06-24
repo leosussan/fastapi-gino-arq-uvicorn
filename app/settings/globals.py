3dc3b5d855a543d17b16e5ff04d6c1ab0a929186
Leo Sussan <leo@reach.vote>
2019-06-23 22:54:10 -0400

    docs: .env file refactor: DB global changed to DATABASE

--- app/settings/globals.py
+++ app/settings/globals.py
@@ -9,12 +9,12 @@ from ..models.pydantic.database import DatabaseURL
 p: Path = Path(__file__).parents[2] / '.env'
 config: Config = Config(p if p.exists() else None)
 
-DB: str = config('DB', cast=str)
+DATABASE: str = config('DATABASE', cast=str)
 DB_USER: Optional[str] = config('DB_USER', cast=str, default=None)
 DB_PASSWORD: Optional[Secret] = config('DB_PASSWORD', cast=Secret, default=None)
 DB_HOST: str = config('DB_HOST', cast=str, default='localhost')
 DB_PORT: int = config('DB_PORT', cast=int, default=5432)
 DATABASE_CONFIG: DatabaseURL = DatabaseURL(drivername="asyncpg", username=DB_USER, password=DB_PASSWORD, host=DB_HOST,
-                                           port=DB_PORT, database=DB)
+                                           port=DB_PORT, database=DATABASE)
 ALEMBIC_CONFIG: DatabaseURL = DatabaseURL(drivername="postgresql+psycopg2", username=DB_USER, password=DB_PASSWORD,
-                                          host=DB_HOST, port=DB_PORT, database=DB)
+                                          host=DB_HOST, port=DB_PORT, database=DATABASE)
