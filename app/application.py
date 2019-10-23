a9fa1910365bf035aa51c469cf8c717ef5281a4d
Leo Sussan <leo@reach.vote>
2019-10-23 18:13:12 -0400

    feat: `SENTRY_DSN` global feat: initialize sentry in `application.py` feat: `SentryAsgiMiddleware` for `main.py`

--- app/application.py
+++ app/application.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from gino.ext.starlette import Gino
+from sentry_sdk import init as initialize_sentry
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from sqlalchemy.schema import MetaData
 
-from .settings.globals import DATABASE_CONFIG
+from .settings.globals import DATABASE_CONFIG, SENTRY_DSN
+
+if SENTRY_DSN not in (None, ""):
+    initialize_sentry(dsn=SENTRY_DSN, integrations=[SqlalchemyIntegration()])
 
 app: FastAPI = FastAPI()
 db: MetaData = Gino(app, dsn=DATABASE_CONFIG.url)
--- app/settings/globals.py
+++ app/settings/globals.py
@@ -6,15 +6,31 @@ from starlette.datastructures import Secret
 
 from ..models.pydantic.database import DatabaseURL
 
-p: Path = Path(__file__).parents[2] / '.env'
+p: Path = Path(__file__).parents[2] / ".env"
 config: Config = Config(p if p.exists() else None)
 
-DATABASE: str = config('DATABASE', cast=str)
-DB_USER: Optional[str] = config('DB_USER', cast=str, default=None)
-DB_PASSWORD: Optional[Secret] = config('DB_PASSWORD', cast=Secret, default=None)
-DB_HOST: str = config('DB_HOST', cast=str, default='localhost')
-DB_PORT: int = config('DB_PORT', cast=int, default=5432)
-DATABASE_CONFIG: DatabaseURL = DatabaseURL(drivername="asyncpg", username=DB_USER, password=DB_PASSWORD, host=DB_HOST,
-                                           port=DB_PORT, database=DATABASE)
-ALEMBIC_CONFIG: DatabaseURL = DatabaseURL(drivername="postgresql+psycopg2", username=DB_USER, password=DB_PASSWORD,
-                                          host=DB_HOST, port=DB_PORT, database=DATABASE)
+DATABASE: str = config("DATABASE", cast=str)
+DB_USER: Optional[str] = config("DB_USER", cast=str, default=None)
+DB_PASSWORD: Optional[Secret] = config(
+    "DB_PASSWORD", cast=Secret, default=None
+)
+DB_HOST: str = config("DB_HOST", cast=str, default="localhost")
+DB_PORT: int = config("DB_PORT", cast=int, default=5432)
+DATABASE_CONFIG: DatabaseURL = DatabaseURL(
+    drivername="asyncpg",
+    username=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DATABASE,
+)
+ALEMBIC_CONFIG: DatabaseURL = DatabaseURL(
+    drivername="postgresql+psycopg2",
+    username=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DATABASE,
+)
+
+SENTRY_DSN: Optional[Secret] = config("SENTRY_DSN", cast=Secret, default=None)
