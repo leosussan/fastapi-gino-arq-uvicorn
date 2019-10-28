0ad1b7c2655ddd633adc4d7204be34597ee27819
Leo Sussan <leo@reach.vote>
2019-10-28 14:39:23 -0400

    feat: `app/tasks/messaging.py`

---
+++ app/tasks/messaging.py
@@ -0,0 +1,15 @@
+from ..main import db
+from ..models.pydantic.user import User
+from ..models.orm.user import User as ORMUser
+
+
+async def send_message(ctx: dict, user_id: int, message: str):
+    # TODO: Add a messaging service.
+    orm_user: ORMUser = await ORMUser.get(user_id)
+    user: User = User.from_orm(orm_user)
+    print(
+        "Message sent to {phone_number}: {message}".format(
+            phone_number=user.phone_number, message=message
+        )
+    )
+    return user
