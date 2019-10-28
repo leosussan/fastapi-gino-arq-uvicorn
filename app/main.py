8062a487c30f9a8b0098c845f5b1cbb40cdd4293
Leo Sussan <leosussan@gmail.com>
2019-10-28 16:23:32 -0400

    Sentry Integration (#7)
    * dept: sentry-sdk added
    * feat: `SENTRY_DSN` global
    feat: initialize sentry in `application.py`
    feat: `SentryAsgiMiddleware` for `main.py`

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import sys
 
 sys.path.extend(["./"])
+
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
 from app.application import app
 from app.routes.users import router as user_router
+from app.settings.globals import SENTRY_DSN
+
 
 ROUTERS = (user_router,)
 
 for r in ROUTERS:
     app.include_router(r)
+
+if SENTRY_DSN not in (None, "", " "):
+    app.add_middleware(SentryAsgiMiddleware)
+
 
 if __name__ == "__main__":
     import uvicorn
